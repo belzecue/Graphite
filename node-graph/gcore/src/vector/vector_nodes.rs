5acc940af7fba3226bff4786b7baf100dc3bca06
Keavon Chambers <keavon@keavon.com>
2025-03-09 16:47:54 -0700

    WIP

--- node-graph/gcore/src/vector/vector_nodes.rs
+++ node-graph/gcore/src/vector/vector_nodes.rs
@@ -620,6 +620,73 @@ async fn sample_points(_: impl Ctx, vector_data: VectorDataTable, spacing: f64, 
 	result
 }
 
+#[node_macro::node(name("Position on Path"), category("Vector"), path(graphene_core::vector))]
+async fn position_on_path(
+	_: impl Ctx,
+	/// The path to traverse.
+	vector_data: VectorDataTable,
+	/// The factor from the start to the end of the path, 0–1 for one subpath, 1–2 for a second subpath, and so on.
+	progress: f64,
+	/// Swap the direction of the path.
+	reverse: bool,
+	/// Traverse the path using the Euclidean distance instead of the parametric distance. Respects actual distances but slower to compute.
+	euclidian: bool,
+) -> DVec2 {
+	let vector_data_transform = vector_data.transform();
+	let vector_data = vector_data.one_instance().instance;
+
+	let subpaths_count = vector_data.stroke_bezier_paths().count() as f64;
+	let progress = progress.clamp(0., subpaths_count);
+	let progress = if reverse { subpaths_count - progress } else { progress };
+	let index = if progress >= subpaths_count { (subpaths_count - 1.) as usize } else { progress as usize };
+
+	vector_data.stroke_bezier_paths().nth(index).map_or(DVec2::ZERO, |mut subpath| {
+		subpath.apply_transform(vector_data_transform);
+
+		let t = if progress == subpaths_count { 1. } else { progress.fract() };
+		subpath.evaluate(if euclidian { SubpathTValue::GlobalEuclidean(t) } else { SubpathTValue::GlobalParametric(t) })
+	})
+}
+
+#[node_macro::node(name("Tangent on Path"), category("Vector"), path(graphene_core::vector))]
+async fn tangent_on_path(
+	_: impl Ctx,
+	/// The path to traverse.
+	vector_data: VectorDataTable,
+	/// The factor from the start to the end of the path, 0–1 for one subpath, 1–2 for a second subpath, and so on.
+	progress: f64,
+	/// Swap the direction of the path.
+	reverse: bool,
+	/// Traverse the path using the Euclidean distance instead of the parametric distance. Respects actual distances but slower to compute.
+	#[default(true)]
+	euclidian: bool,
+) -> f64 {
+	let vector_data_transform = vector_data.transform();
+	let vector_data = vector_data.one_instance().instance;
+
+	let subpaths_count = vector_data.stroke_bezier_paths().count() as f64;
+	let progress = progress.clamp(0., subpaths_count);
+	let progress = if reverse { subpaths_count - progress } else { progress };
+	let index = if progress >= subpaths_count { (subpaths_count - 1.) as usize } else { progress as usize };
+
+	vector_data.stroke_bezier_paths().nth(index).map_or(0., |mut subpath| {
+		subpath.apply_transform(vector_data_transform);
+
+		let t = if progress == subpaths_count { 1. } else { progress.fract() };
+		let mut tangent = subpath.tangent(if euclidian { SubpathTValue::GlobalEuclidean(t) } else { SubpathTValue::GlobalParametric(t) });
+		if tangent == DVec2::ZERO {
+			let t = t + if t > 0.5 { -0.001 } else { 0.001 };
+			tangent = subpath.tangent(if euclidian { SubpathTValue::GlobalEuclidean(t) } else { SubpathTValue::GlobalParametric(t) });
+		}
+		if tangent == DVec2::ZERO {
+			debug!("Tangent is zero");
+			return 0.;
+		}
+
+		-tangent.angle_to(if reverse { -DVec2::X } else { DVec2::X })
+	})
+}
+
 #[node_macro::node(category(""), path(graphene_core::vector))]
 async fn poisson_disk_points(
 	_: impl Ctx,
